964b248e0becbccb4534b691a5e4c9575c53b75b
Username <username@NI-NTB-118.local>
2017-08-29 18:29:01 -0300

    Adding Unit testing to rectangle class

---
+++ src/test/java/com/demo/app/RectangleTest.java
@@ -0,0 +1,31 @@
+package com.demo.app;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import static org.junit.Assert.assertEquals;
+
+//Comment to test in the test
+public class RectangleTest {
+  Rectangle myRectangle = new Rectangle(5,6);
+
+  @Test
+  public void testGetArea() {
+    assertEquals(myRectangle.getArea(), 30);
+  }
+
+  @Test
+  public void testGetPerimeter() {
+    assertEquals(myRectangle.getPerimeter(), 22);
+  }
+
+  @Test
+  public void testLength() {
+    assertEquals(myRectangle.length, 5);
+  }
+
+  @Test
+  public void testWidth() {
+    assertEquals(myRectangle.width, 6);
+  }
+}
+
