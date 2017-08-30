e29a335d9dff4e0f21ef21da2a006ad17f827e40
Username <username@NI-NTB-118.local>
2017-08-30 11:43:57 -0300

    Modified test to check if the application loads correctly

--- src/test/java/com/demo/app/AppApplicationTests.java
+++ src/test/java/com/demo/app/AppApplicationTests.java
@@ -2,15 +2,21 @@ package com.demo.app;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import static org.assertj.core.api.Assertions.assertThat;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class AppApplicationTests {
-
+    
+    @Autowired
+    private rectangleController controller;
+	
 	@Test
 	public void contextLoads() {
+		assertThat(controller).isNotNull();
 	}
 
 }
